c1dd3b6727cacb21ca89ea1c7a9f7593f352aad1
jeonghyeonji <chunghj09@gmail.com>
2017-11-29 18:59:32 +0900

    set coordinator UI

--- app/src/main/java/com/hyundeee/app/wingsui/MainActivity.kt
+++ app/src/main/java/com/hyundeee/app/wingsui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.hyundeee.app.wingsui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_coordinator.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_coordinator)
+        collapsing_toolbar.apply {
+            title = "이스타" //TODO : 기존 플레이윙즈 UI와 같이 항공사 이름이 보이도록 한다
+            setExpandedTitleColor(resources.getColor(android.R.color.transparent))
+        }
     }
 }
