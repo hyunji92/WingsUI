fa4d90d15582b5072531f95d079e35d002c1a79d
jeonghyeonji <chunghj09@gmail.com>
2017-12-01 03:20:01 +0900

    set Wings UI

--- app/src/main/java/com/hyundeee/app/wingsui/MainActivity.kt
+++ app/src/main/java/com/hyundeee/app/wingsui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.hyundeee.app.wingsui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.util.Linkify
 import kotlinx.android.synthetic.main.activity_main_coordinator.*
+import kotlinx.android.synthetic.main.faq_card_view.*
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         collapsing_toolbar.apply {
             title = "이스타" //TODO : 기존 플레이윙즈 UI와 같이 항공사 이름이 보이도록 한다
             setExpandedTitleColor(resources.getColor(android.R.color.transparent))
+
+            val mTransform:Linkify.TransformFilter = Linkify.TransformFilter { _, _ -> "" }
+            val pattern = Pattern.compile(resources.getString(R.string.beginner_reservation))
+
+            Linkify.addLinks(beginner_reservation_text,Linkify.WEB_URLS)
         }
     }
 }
