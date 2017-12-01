b9f7e57b6fc930ca6fb4d1757fb17c5e0eb3ebd8
jeonghyeonji <chunghj09@gmail.com>
2017-12-01 11:03:16 +0900

    style 수정

--- app/src/main/java/com/hyundeee/app/wingsui/BaseToolbarActivity.kt
+++ app/src/main/java/com/hyundeee/app/wingsui/BaseToolbarActivity.kt
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main_coordinator.*
+import android.support.design.widget.CollapsingToolbarLayout
+
+
 
 /**
  * Created by ${hyundee} on 2017. 12. 1..
@@ -31,6 +34,18 @@ open class BaseToolbarActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setSupportActionBar(toolbar)
         }
+
+
+        /*if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            var marginResult = 0
+            val resourceId = resources.getIdentifier(getString(R.string.identifier_status_bar_height), getString(R.string.identifier_dimen), getString(R.string.identifier_android))
+            if (resourceId > 0) {
+                marginResult = resources.getDimensionPixelSize(resourceId) * 2
+            }
+            val params = collapsing_toolbar.layoutParams as CollapsingToolbarLayout.LayoutParams
+            params.topMargin -= marginResult
+            collapsing_toolbar.layoutParams = params
+        }*/
     }
 
     private fun initStatusBarColor() {
