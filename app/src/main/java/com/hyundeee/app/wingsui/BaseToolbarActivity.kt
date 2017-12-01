990d139a1d15c03babd3afa5865df230f4e834f1
jeonghyeonji <chunghj09@gmail.com>
2017-12-01 10:32:29 +0900

    add Wings UI BaseToolbarActivity

---
+++ app/src/main/java/com/hyundeee/app/wingsui/BaseToolbarActivity.kt
@@ -0,0 +1,55 @@
+package com.hyundeee.app.wingsui
+
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main_coordinator.*
+
+/**
+ * Created by ${hyundee} on 2017. 12. 1..
+ */
+open class BaseToolbarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        // 상단바 설정
+        this.initStatusBarColor()
+    }
+
+    override fun setContentView(layoutResID: Int) {
+        if (layoutResID != -1) super.setContentView(layoutResID)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setSupportActionBar(toolbar)
+        }
+    }
+
+    private fun initStatusBarColor() {
+        val window = window
+
+        // 롤리팝 이상인 OS를 사용할 경우 상단바의 색상을 변경한다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ResourcesCompat.getColor(resources, R.color.white, theme)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+
+        // 마시멜로우 이상일때 상단바 아이템의 색상을 어두운 색으로 변경한다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+
+}
--- app/src/main/java/com/hyundeee/app/wingsui/MainActivity.kt
+++ app/src/main/java/com/hyundeee/app/wingsui/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.hyundeee.app.wingsui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.util.Linkify
 import kotlinx.android.synthetic.main.activity_main_coordinator.*
-import kotlinx.android.synthetic.main.faq_card_view.*
-import java.util.regex.Pattern
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +12,6 @@ class MainActivity : AppCompatActivity() {
             title = "이스타" //TODO : 기존 플레이윙즈 UI와 같이 항공사 이름이 보이도록 한다
             setExpandedTitleColor(resources.getColor(android.R.color.transparent))
 
-            val mTransform:Linkify.TransformFilter = Linkify.TransformFilter { _, _ -> "" }
-            val pattern = Pattern.compile(resources.getString(R.string.beginner_reservation))
-
-            Linkify.addLinks(beginner_reservation_text,Linkify.WEB_URLS)
         }
     }
 }
